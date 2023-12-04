de9a6ea760cb75c3448a1aca0ed0d1e57173ee1d
maik <estevan.maik@gmail.com>
2023-12-04 19:08:12 -0300

    feat: adds Point class

---
+++ tupl/Point.cs
@@ -0,0 +1,11 @@
+public class Point : Tupl
+{
+  public Point() : base(0f, 0f, 0f, 1f)
+  {
+  }
+
+  public Point(float x = 0f, float y = 0f, float z = 0f, float w = 1f) : base(x, y, z, w)
+  {
+  }
+}
+
