e888b71499a46cd74392273e022d83c960b53e1c
maik <estevan.maik@gmail.com>
2023-12-04 19:07:06 -0300

    feat: adds custom tuple

---
+++ tupl/Tupl.cs
@@ -0,0 +1,16 @@
+public abstract class Tupl
+{
+  public float x;
+  public float y;
+  public float z;
+  public float w;
+
+  public Tupl(float x, float y, float z, float w)
+  {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.w = w;
+  }
+}
+
