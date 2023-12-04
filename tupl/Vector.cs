9312dde984b4851cf80e08308252659d9a2221b5
maik <estevan.maik@gmail.com>
2023-12-04 19:07:54 -0300

    feat: adds Vector class

---
+++ tupl/Vector.cs
@@ -0,0 +1,11 @@
+public class Vector : Tupl
+{
+  public Vector() : base(0f, 0f, 0f, 0f)
+  {
+  }
+
+  public Vector(float x = 0f, float y = 0f, float z = 0f, float w = 0f) : base(x, y, z, w)
+  {
+  }
+}
+
