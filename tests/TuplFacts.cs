c334f521c374dd87368824b9e71f368c9fd76c3a
maik <estevan.maik@gmail.com>
2023-12-05 18:42:02 -0300

    test: adds test coverage for vector and point sum

--- tests/TuplFacts.cs
+++ tests/TuplFacts.cs
@@ -2,7 +2,7 @@ using Xunit;
 
 public class TuplFacts
 {
-  public class CreateVector
+  public class Vectors
   {
     [Fact]
     public void VectorWReturnsZero()
@@ -13,9 +13,24 @@ public class TuplFacts
 
       Assert.Equal(expectedW, vector.w);
     }
+
+    [Fact]
+    public void VectorsSumReturnsVector()
+    {
+      float expectedW = 0f;
+
+      Vector expectedVector = new(2, 3, 5);
+      Vector vectorA = new(3, 1, 2);
+      Vector vectorB = new(-1, 2, 3);
+
+      Vector resultVector = vectorA + vectorB;
+
+      Assert.Equivalent(expectedVector, resultVector);
+      Assert.Equal(expectedW, resultVector.w);
+    }
   }
 
-  public class CreatePoint
+  public class Points
   {
     [Fact]
     public void PointWReturnsOne()
@@ -25,6 +40,21 @@ public class TuplFacts
       Point point = new();
 
       Assert.Equal(expectedW, point.w);
+    }
+
+    [Fact]
+    public void PointAndVectorSumReturnsPoint()
+    {
+      float expectedW = 1f;
+
+      Point expectedPoint = new(0, 6, 7);
+      Point point = new(1, 4, 4);
+      Vector vector = new(-1, 2, 3);
+
+      Point resultPoint = point + vector;
+
+      Assert.Equivalent(expectedPoint, resultPoint);
+      Assert.Equal(expectedW, resultPoint.w);
     }
   }
 }
