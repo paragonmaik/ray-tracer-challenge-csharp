f04b4bc5af968ec3421d0bd303cfc18df2566834
maik <estevan.maik@gmail.com>
2023-12-05 19:48:01 -0300

    feat: adds point sub operation

--- tupl/Point.cs
+++ tupl/Point.cs
@@ -20,5 +20,27 @@ public class Point : Tupl
 
     return pointSum;
   }
+
+  public static Vector operator -(Point a, Point b)
+  {
+    Vector vectorSubtraction = new(
+        a.x - b.x,
+        a.y - b.y,
+        a.z - b.z,
+        a.w - b.w);
+
+    return vectorSubtraction;
+  }
+
+  public static Point operator -(Point a, Vector b)
+  {
+    Point pointSub = new(
+        a.x - b.x,
+        a.y - b.y,
+        a.z - b.z,
+        a.w - b.w);
+
+    return pointSub;
+  }
 }
 
