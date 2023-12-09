91b75304cd9268f132a1707ef4c5c79d4291a695
maik <estevan.maik@gmail.com>
2023-12-09 16:54:27 -0300

    chore: Environment and Projectile constructors are now public

--- Environment.cs
+++ Environment.cs
@@ -3,7 +3,7 @@ public class Environment
   public Vector gravity;
   public Vector wind;
 
-  Environment(Vector gravity, Vector wind)
+  public Environment(Vector gravity, Vector wind)
   {
     this.gravity = gravity;
     this.wind = wind;
--- Projectile.cs
+++ Projectile.cs
@@ -3,7 +3,7 @@ public class Projectile
   public Point position;
   public Vector velocity;
 
-  Projectile(Point position, Vector velocity)
+  public Projectile(Point position, Vector velocity)
   {
     this.position = position;
     this.velocity = velocity;
