64d9708bf3727ed267c41d62db9db398a4efc972
maik <estevan.maik@gmail.com>
2023-12-09 19:05:36 -0300

    feat: adds Run method

--- Program.cs
+++ Program.cs
@@ -1,8 +1,35 @@
-﻿
-public class Program
+﻿class Program
 {
-  public void Tick()
+  public static void Tick(Environment env, Projectile proj)
   {
+    proj.position = proj.position + proj.velocity;
+    proj.velocity = proj.velocity + env.gravity + env.wind;
+  }
 
+  public static void Run()
+  {
+    Vector gravity = new(0, -0.1f, 0);
+    Vector wind = new(-0.01f, 0, 0);
+
+    Point position = new(0, 1, 0);
+    Vector velocity = new(1, 1, 0);
+
+    Environment env = new(gravity, wind);
+    Projectile proj = new(position, velocity);
+
+
+    while (proj.position.y > 0f)
+    {
+      Tick(env, proj);
+      Console.WriteLine(
+                    $"{proj.position.x},{proj.position.y},{proj.position.z}");
+
+    }
+  }
+
+  static void Main(string[] args)
+  {
+    Run();
   }
 }
+
