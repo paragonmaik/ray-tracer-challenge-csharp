cc30446ca710e1330c33c5dbdc5c0e785a297f8e
maik <estevan.maik@gmail.com>
2023-12-10 16:26:10 -0300

    test: adds SetPixel testing coverage

--- tests/CanvasFacts.cs
+++ tests/CanvasFacts.cs
@@ -17,5 +17,18 @@ public class CanvasFacts
       Assert.Equal(canvas.GetWidth(), height);
       Assert.Equivalent(defaultCanvasColor, canvas.canvas[0, 0]);
     }
+
+    [Fact]
+    public void ValidateSetPixelMethod()
+    {
+      int width = 10;
+      int height = 10;
+
+      Canvas canvas = new(width, height);
+      Color white = new(1, 1, 1);
+      canvas.SetPixel(4, 4, white);
+
+      Assert.Equivalent(white, canvas.canvas[4, 4]);
+    }
   }
 }
