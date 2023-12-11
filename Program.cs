f6d360564d8a8333bf6e5dceb9ab1841278c630d
maik <estevan.maik@gmail.com>
2023-12-11 17:09:12 -0300

    chore: runs Save method to generate ppm file

--- Program.cs
+++ Program.cs
@@ -12,11 +12,14 @@
     Vector wind = new(-0.01f, 0, 0);
 
     Point position = new(0, 1, 0);
-    Vector velocity = new(1, 1, 0);
+    Vector velocity = new(6, 6, 0);
 
     Environment env = new(gravity, wind);
     Projectile proj = new(position, velocity);
 
+    Canvas canvas = new(500, 500);
+    canvas.FillCanvas(new(1, 1, 1));
+    Color redColor = new(1, 0.5f, 0.5f);
 
     while (proj.position.y > 0f)
     {
@@ -24,7 +27,10 @@
       Console.WriteLine(
                     $"{proj.position.x},{proj.position.y},{proj.position.z}");
 
+      canvas.SetPixel(
+          (int)proj.position.x, (int)proj.position.y, redColor);
     }
+    canvas.Save("file");
   }
 
   static void Main(string[] args)
