a7980b2cf7c81c2843f6f19965d8ba8ed119c415
maik <estevan.maik@gmail.com>
2023-12-13 09:59:10 -0300

    test: adds tests to matrices of size 3 and 2

--- tests/MatrixFacts.cs
+++ tests/MatrixFacts.cs
@@ -5,7 +5,7 @@ public class MatrixFacts
   public class Matrices
   {
     [Fact]
-    public void ValidateMatrixInstantiation()
+    public void ValidateMatrix4x4Instantiation()
     {
       int size = 4;
       double randomVal = 0.4f;
@@ -15,6 +15,32 @@ public class MatrixFacts
 
       Assert.Equal(mat4.GetSize(), size);
       Assert.Equal(mat4[0, 3], randomVal);
+    }
+
+    [Fact]
+    public void ValidateMatrix3x3Instantiation()
+    {
+      int size = 3;
+      double randomVal = 0.9f;
+      Matrix mat3 = new(size);
+
+      mat3[0, 2] = randomVal;
+
+      Assert.Equal(mat3.GetSize(), size);
+      Assert.Equal(mat3[0, 2], randomVal);
+    }
+
+    [Fact]
+    public void ValidateMatrix2x2Instantiation()
+    {
+      int size = 2;
+      double randomVal = 0.1f;
+      Matrix mat2 = new(size);
+
+      mat2[0, 1] = randomVal;
+
+      Assert.Equal(mat2.GetSize(), size);
+      Assert.Equal(mat2[0, 1], randomVal);
     }
   }
 }
