dae77147731c2a63dc9cfa127f11be18c8e8c8dd
maik <estevan.maik@gmail.com>
2023-12-26 11:42:50 -0300

    feat: adds Sphere class

--- Sphere.cs
+++ Sphere.cs
@@ -1,11 +1,15 @@
 public class Sphere
 {
+  private Point origin;
   private double radius;
   private Guid id;
 
   public Sphere()
   {
+    this.origin = new();
     this.radius = 1f;
     this.id = Guid.NewGuid();
   }
+
+  public Point Origin() { return this.origin; }
 }
