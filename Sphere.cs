cfff2d9f722d8c6beff9fe2ac78687d668ed0e8f
maik <estevan.maik@gmail.com>
2023-12-29 10:31:13 -0300

    feat: adds GetMatrix and SetMatrix methods to the Sphere class

--- Sphere.cs
+++ Sphere.cs
@@ -3,15 +3,19 @@ public class Sphere : IIntersectable
   private Point origin;
   private double radius;
   private Guid id;
+  private Matrix mat;
 
   public Sphere()
   {
     this.origin = new();
     this.radius = 1f;
     this.id = Guid.NewGuid();
+    this.mat = new Matrix(4).Identity();
   }
 
   public Point Origin() { return this.origin; }
+  public Matrix GetMatrix() { return this.mat; }
+  public void SetMatrix(Matrix mat) { this.mat = mat; }
 
   public override bool Equals(object? obj)
   {
