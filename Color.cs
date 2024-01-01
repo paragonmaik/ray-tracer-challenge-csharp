f0ae57f97a1107fe4d78839b3cb83d304a08e4d1
maik <estevan.maik@gmail.com>
2024-01-01 14:00:07 -0300

    feat: adds overridden Equals and ToString method to the Color class

--- Color.cs
+++ Color.cs
@@ -5,8 +5,7 @@ public class Color
   public float blue;
 
   public Color()
-  {
-  }
+  { }
 
   public Color(float red = 0f, float green = 0f,
       float blue = 0f)
@@ -50,5 +49,42 @@ public class Color
         a.green * b,
         a.blue * b
         );
+  }
+
+  public override bool Equals(object? obj)
+  {
+    if (obj == null)
+    {
+      return false;
+    }
+
+    Color color = (Color)obj;
+
+    if (this.red != color.red)
+    {
+      return false;
+    }
+
+    if (this.green != color.green)
+    {
+      return false;
+    }
+
+    if (this.blue != color.blue)
+    {
+      return false;
+    }
+
+    return true;
+  }
+
+  public override int GetHashCode()
+  {
+    return base.GetHashCode();
+  }
+
+  public override string ToString()
+  {
+    return $"Red: {this.red}, Green: {this.green}, Blue: {this.blue}";
   }
 }
