31edc21f4adf223ecc41f78e5b008bdb558ef157
maik <estevan.maik@gmail.com>
2024-01-01 11:27:00 -0300

    test: added tests to validadte tranformed spheres normal

--- tests/SphereFacts.cs
+++ tests/SphereFacts.cs
@@ -18,5 +18,34 @@ public class SphereFacts
 
       Assert.Equal(expectedVector, actualNormalizedVector);
     }
+
+    [Fact]
+    public void ValidateTranslatedSphereNormal()
+    {
+      Sphere sphere = new();
+      Vector expectedVector = new(0, 0.70711f, -0.70711f);
+      sphere.SetMatrix(sphere.GetMatrix().Translate(0, 1, 0));
+
+      Vector actualNormalizedVector = sphere
+        .NormalAt(new(0, 1.70711f, -0.70711f));
+
+      Assert.Equal(expectedVector, actualNormalizedVector);
+    }
+
+    [Fact]
+    public void ValidateScaledSphereNormal()
+    {
+      Sphere sphere = new();
+      Vector expectedVector = new(0, 0.97014f, -0.24254f);
+      sphere.SetMatrix(
+          sphere.GetMatrix().Scale(1, 0.5f, 1) *
+          sphere.GetMatrix().RotateZAxis(Math.PI / 5)
+          );
+
+      Vector actualNormalizedVector = sphere
+        .NormalAt(new(0, (float)Math.Sqrt(2) / 2, -(float)Math.Sqrt(2) / 2));
+
+      Assert.Equal(expectedVector, actualNormalizedVector);
+    }
   }
 }
