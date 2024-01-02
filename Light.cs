9a0326726c00cf61bd356191b20435b59c7f9285
maik <estevan.maik@gmail.com>
2024-01-02 15:24:23 -0300

    feat: adds Lighting method

--- Light.cs
+++ Light.cs
@@ -9,6 +9,47 @@ public class Light
     this.position = position;
   }
 
+  public Color Lighting(
+      Material ma, Point p, Vector eyeV, Vector normalV)
+  {
+    Material m = new(new(1, 1, 1));
+    Color diffuseColor = new();
+    Color specularColor = new();
+
+    Color effectiveColor = m.color * this.intensity;
+    Vector lightV = (this.position - p).Normalize();
+    Color ambientColor = effectiveColor * (float)m.Ambient;
+    double lightDotNormal = lightV.Dot(normalV);
+
+    if (lightDotNormal < 0)
+    {
+      diffuseColor = new(0, 0, 0);
+      specularColor = new(0, 0, 0);
+    }
+    else
+    {
+      diffuseColor = effectiveColor *
+        (float)m.Diffuse * (float)lightDotNormal;
+      Vector reflectV = new Vector()
+        .Reflect(-lightV, normalV);
+      double reflectDotEye = reflectV.Dot(eyeV);
+
+      if (reflectDotEye <= 0)
+      {
+        specularColor = new(0, 0, 0);
+      }
+      else
+      {
+        double factor = (double)Math
+          .Pow(reflectDotEye, m.Shininess);
+        specularColor = this.intensity *
+          (float)m.Specular * (float)factor;
+      }
+    }
+
+    return ambientColor + diffuseColor + specularColor;
+  }
+
   public override string ToString()
   {
     return $"Intensity: {this.intensity}, Position: {this.position}";
