9d36252dcb8ed3ea3b94463353db0d70bd7917d1
maik <estevan.maik@gmail.com>
2024-01-05 13:50:10 -0300

    feat: adds IntersectWorld method

--- Scene.cs
+++ Scene.cs
@@ -1,7 +1,18 @@
 public class Scene
 {
+  // TODO: add list that concatenates all IntersectableObjects
+  // iterate through all of them to invoke the Intersect
+  // method passed to the IntersectWorld method
+  public Light light;
+  public Sphere innerSphere;
+  public Sphere outerSphere;
+
   public Scene()
   {
+    this.light = new(new(1, 1, 1), new(-10, 10, -10));
+    this.innerSphere = new();
+    this.outerSphere = new();
+    DefaultScene();
   }
 
   public Intersection Hit(List<Intersection> intersections)
@@ -22,5 +33,39 @@ public class Scene
     }
 
     return firstHit;
+  }
+
+  public List<Intersection> IntersectWorld(Ray ray)
+  {
+    List<Intersection> intersections = new();
+    List<Intersection> innerIntersections = innerSphere
+      .Intersect(ray);
+    List<Intersection> outerIntersections = outerSphere
+      .Intersect(ray);
+
+    intersections.Add(innerIntersections[0]);
+    intersections.Add(innerIntersections[1]);
+    intersections.Add(outerIntersections[0]);
+    intersections.Add(outerIntersections[1]);
+
+    List<Intersection> sortedIntersections = intersections
+      .OrderBy(i => i.t).ToList();
+
+    return sortedIntersections;
+  }
+
+  private void DefaultScene()
+  {
+    this.innerSphere = new();
+    Material mat = new(new(0.8f, 1, 0.6f));
+
+    mat.Diffuse = 0.7f;
+    mat.Specular = 0.2f;
+    innerSphere.material = mat;
+
+    this.outerSphere
+      .SetMatrix(this.outerSphere
+      .GetMatrix()
+      .Scale(0.5f, 0.5f, 0.5f));
   }
 }
