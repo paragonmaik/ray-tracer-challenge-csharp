52f47f72a2bdb27b5309dbb5eeaba948ec9d780d
maik <estevan.maik@gmail.com>
2024-01-06 10:46:34 -0300

    test: adds test to validate PrepareComputations

---
+++ tests/ComputationsFacts.cs
@@ -0,0 +1,25 @@
+using Xunit;
+
+public class ComputationsFacts
+{
+  public class Computation
+  {
+    [Fact]
+    public void ValidatePrepareComputations()
+    {
+      Scene scene = new();
+      Ray ray = new(new(0, 0, -5), new(0, 0, 1));
+      Sphere sphere = new();
+      List<Intersection> intersections = sphere.Intersect(ray);
+      Intersection firstHit = scene.Hit(intersections);
+
+      Computations c = new(firstHit, ray);
+
+      Assert.Equal(firstHit.t, c.t);
+      Assert.Equal(firstHit.intersectedObj, c.intersectedObject);
+      Assert.Equal(new(0, 0, -1), c.point);
+      Assert.Equal(new(0, 0, -1), c.eyeV);
+      Assert.Equal(new(0, 0, -1), c.normalV);
+    }
+  }
+}
