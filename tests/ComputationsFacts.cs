b6babf1763fd9efbd88b747f1fcd1963fa4dda91
maik <estevan.maik@gmail.com>
2024-01-06 16:12:17 -0300

    test: adds test to validate inside and outside shading

--- tests/ComputationsFacts.cs
+++ tests/ComputationsFacts.cs
@@ -49,5 +49,39 @@ public class ComputationsFacts
 
       Assert.Equal(true, c.inside);
     }
+
+    [Fact]
+    public void ValidateShadingOutsideHit()
+    {
+      // TODO: use scene methods
+      Scene scene = new();
+      Ray ray = new(new(0, 0, -5), new(0, 0, 1));
+      Sphere sphere = scene.innerSphere;
+
+      List<Intersection> intersections = sphere.Intersect(ray);
+      Intersection firstHit = scene.Hit(intersections);
+      Computations c = new(firstHit, ray);
+
+      Color color = scene.ShadeHit(c);
+
+      Assert.Equal(new(0.38066f, 0.47583f, 0.2855f), color);
+    }
+
+    [Fact]
+    public void ValidateShadingInsideHit()
+    {
+      Scene scene = new();
+      scene.light = new(new(1, 1, 1), new(0, 0.25f, 0));
+      Ray ray = new(new(0, 0, 0), new(0, 0, 1));
+      Sphere sphere = scene.outerSphere;
+
+      List<Intersection> intersections = sphere.Intersect(ray);
+      Intersection firstHit = scene.Hit(intersections);
+      Computations c = new(firstHit, ray);
+
+      Color color = scene.ShadeHit(c);
+
+      Assert.Equal(new(0.90498f, 0.90498f, 0.90498f), color);
+    }
   }
 }
