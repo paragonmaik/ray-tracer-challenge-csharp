3c0922cb949cb2b7913683e964dc60244cd41734
maik <estevan.maik@gmail.com>
2024-01-07 10:21:38 -0300

    test: adds test to validate ViewTransform Camera method

---
+++ tests/CameraFacts.cs
@@ -0,0 +1,64 @@
+using Xunit;
+
+public class CameraFacts
+{
+  public class Cameras
+  {
+    [Fact]
+    public void ValidateDefaultMatrixOrientation()
+    {
+      Camera cam = new();
+      Point from = new(0, 0, 0);
+      Point to = new(0, 0, -1);
+      Vector up = new(0, 1, 0);
+      Matrix expectedViewTransform = new Matrix(4)
+        .Identity();
+
+      Matrix actualViewTransform = cam
+        .ViewTransform(from, to, up);
+
+      Assert.Equal(expectedViewTransform, actualViewTransform);
+    }
+
+    [Fact]
+    public void ValidateViewTransformZDirection()
+    {
+      Camera cam = new();
+      Point from = new(0, 0, 0);
+      Point to = new(0, 0, 1);
+      Vector up = new(0, 1, 0);
+      Matrix expectedViewTransform = new Matrix(4)
+        .Scale(-1, -1, -1);
+
+      Matrix actualViewTransform = cam
+        .ViewTransform(from, to, up);
+
+      Assert.Equal(expectedViewTransform, actualViewTransform);
+    }
+
+    [Fact]
+    public void ValidateViewTransformsMoveScene()
+    {
+      Camera cam = new();
+      Point from = new(0, 0, 0);
+      Point to = new(0, 0, 0);
+      Vector up = new(0, 1, 0); Matrix expectedViewTransform = new Matrix(4).Translate(0, 0, -8); Matrix actualViewTransform = cam.ViewTransform(from, to, up); Assert.Equal(expectedViewTransform, actualViewTransform);
+    }
+    [Fact]
+    public void ValidateArbitraryViewTransformation()
+    {
+      Camera cam = new(); Point from = new(1, 3, 2); Point to = new(4, -2, 8); Vector up = new(1, 1, 0);
+      Matrix expectedViewTransform = new(new double[4, 4]{
+       { -0.50709 , 0.50709 , 0.67612 , -2.36643 },
+       { 0.76772 , 0.60609 , 0.12122 , -2.82843 },
+       { -0.35857 , 0.59761 , -0.71714 , 0.00000 },
+       { 0.00000 , 0.00000 , 0.00000 , 1.00000 }
+      });
+
+      Matrix actualViewTransform = cam
+        .ViewTransform(from, to, up);
+
+      Assert.Equal(expectedViewTransform, actualViewTransform);
+    }
+  }
+}
