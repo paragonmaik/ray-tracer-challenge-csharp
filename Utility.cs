6c2201512875ad6f728cb9c063f3f3abc576af7a
maik <estevan.maik@gmail.com>
2024-01-19 18:02:54 -0300

    feat: adds Utility class

---
+++ Utility.cs
@@ -0,0 +1,21 @@
+﻿public static class Utility
+{
+  public static double epsilon = 0.001;
+  public static double Infinity = 1e10;
+
+  public static double DegToRad(double degrees)
+  {
+    return degrees * (Math.PI / 180.0);
+  }
+
+  public static bool FE(double a, double b)
+  {
+    double temp = Math.Abs(a - b);
+
+    if (temp < epsilon)
+    {
+      return true;
+    }
+    return false;
+  }
+}
