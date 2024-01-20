88c0b064b3827fd98ffa464e143fae55d0a1705e
maik <estevan.maik@gmail.com>
2024-01-20 10:28:48 -0300

    refactor: reworks Point.cs

--- Point.cs
+++ Point.cs
@@ -1,84 +1,66 @@
-public class Point : Tupl
+﻿public class Point : Tuple
 {
-  public Point() : base(0, 0, 0, 1)
-  {
-  }
+  public Point()
+    : base(0, 0, 0, 1) { }
 
-  public Point(double x = 0, double y = 0,
-      double z = 0, double w = 1) : base(x, y, z, w)
+  public Point(Point p)
+    : base(p.x, p.y, p.z, p.w) { }
+
+  public Point(
+    double x = 0,
+    double y = 0,
+    double z = 0,
+    double w = 1
+  )
+    : base(x, y, z, w)
   {
     this.w = 1f;
   }
 
   public static Point operator +(Point a, Vector b)
   {
-    Point pointSum = new(
-        a.x + b.x,
-        a.y + b.y,
-        a.z + b.z,
-        a.w + b.w);
+    Point pointSum =
+      new(a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w);
 
     return pointSum;
   }
 
   public static Vector operator -(Point a, Point b)
   {
-    Vector vectorSubtraction = new(
-        a.x - b.x,
-        a.y - b.y,
-        a.z - b.z,
-        a.w - b.w);
+    Vector vectorSubtraction =
+      new(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w);
 
     return vectorSubtraction;
   }
 
   public static Point operator -(Point a, Vector b)
   {
-    Point pointSub = new(
-        a.x - b.x,
-        a.y - b.y,
-        a.z - b.z,
-        a.w - b.w);
+    Point pointSub =
+      new(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w);
 
     return pointSub;
   }
 
   public static Point operator -(Vector a, Point b)
   {
-    Point pointSub = new(
-        a.x - b.x,
-        a.y - b.y,
-        a.z - b.z,
-        a.w - b.w);
+    Point pointSub =
+      new(a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w);
 
     return pointSub;
   }
 
   public static Point operator *(Point a, float b)
   {
-    return new Point(
-    a.x * b,
-    a.y * b,
-    a.z * b,
-    a.w * b);
+    return new Point(a.x * b, a.y * b, a.z * b, a.w * b);
   }
 
   public static Point operator /(Point a, float b)
   {
-    return new Point(
-    a.x / b,
-    a.y / b,
-    a.z / b,
-    a.w / b);
+    return new Point(a.x / b, a.y / b, a.z / b, a.w / b);
   }
 
   public static Point operator !(Point a)
   {
-    return new Point(
-        a.x * -1,
-        a.y * -1,
-        a.z * -1,
-        a.w * -1);
+    return new Point(a.x * -1, a.y * -1, a.z * -1, a.w * -1);
   }
 }
-
