f24ac213e11550a6f96833c632c57388a241a9b4
maik <estevan.maik@gmail.com>
2024-01-20 11:36:38 -0300

    refactor: reworks Intersection.cs

--- Intersection.cs
+++ Intersection.cs
@@ -1,11 +1,66 @@
-public class Intersection
+﻿public class Intersection
 {
+  public IntersectableObject intersectableObj;
   public double t;
-  public IntersectableObject intersectedObj;
+  public double u;
+  public double v;
 
-  public Intersection(double t, IntersectableObject intersectedObj)
+  public Intersection(
+    IntersectableObject obj,
+    double t,
+    double u = 0.0,
+    double v = 0.0
+  )
   {
+    intersectableObj = obj;
     this.t = t;
-    this.intersectedObj = intersectedObj;
+    this.u = u;
+    this.v = v;
+  }
+
+  public static List<Intersection> SortIntersections(
+    List<Intersection> intersections
+  )
+  {
+    if (intersections.Count == 0)
+      return intersections;
+
+    List<Intersection> sortedIntersections =
+      new List<Intersection>();
+
+    sortedIntersections.Add(intersections[0]);
+
+    int currentIntersection = 1;
+    bool valueInserted = false;
+
+    while (currentIntersection < intersections.Count)
+    {
+      valueInserted = false;
+      for (int i = 0; i < sortedIntersections.Count; i++)
+      {
+        if (
+          intersections[currentIntersection].t
+          < sortedIntersections[i].t
+        )
+        {
+          sortedIntersections.Insert(
+            i,
+            intersections[currentIntersection]
+          );
+          currentIntersection++;
+          valueInserted = true;
+          break;
+        }
+      }
+      if (!valueInserted)
+      {
+        sortedIntersections.Add(
+          intersections[currentIntersection]
+        );
+        currentIntersection++;
+        valueInserted = false;
+      }
+    }
+    return sortedIntersections;
   }
 }
