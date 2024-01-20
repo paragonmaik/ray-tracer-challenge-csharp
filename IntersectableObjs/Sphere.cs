85f2c270ca6a65858a7d5d62254a868ff5b05250
maik <estevan.maik@gmail.com>
2024-01-20 15:09:34 -0300

    refactor: reformats Sphere.cs

--- IntersectableObjs/Sphere.cs
+++ IntersectableObjs/Sphere.cs
@@ -8,11 +8,6 @@
     this.radius = radius;
   }
 
-  public override string ToString()
-  {
-    return "Sphere: " + id.ToString();
-  }
-
   public override Vector CalculateLocalNormal(
     Point localPoint,
     Intersection i = null
@@ -33,10 +28,12 @@
     Ray transRay = RayToObjectSpace(ray);
 
     Vector sphereToRay = transRay.Origin() - new Point(0, 0, 0);
+
     double a = transRay.Direction().Dot(transRay.Direction());
     double b = 2.0 * transRay.Direction().Dot(sphereToRay);
     double c = sphereToRay.Dot(sphereToRay) - 1.0;
     double discriminant = b * b - 4.0 * a * c;
+
     if (discriminant < 0)
       return intersectionPoints;
 
@@ -44,25 +41,10 @@
       (-b - (double)Math.Sqrt(discriminant)) / (2.0 * a);
     double t2 =
       (-b + (double)Math.Sqrt(discriminant)) / (2.0 * a);
+
     intersectionPoints.Add(new Intersection(this, t1));
     intersectionPoints.Add(new Intersection(this, t2));
 
     return intersectionPoints;
   }
-
-  // public override Bounds GetLocalBounds()
-  // {
-  //   Bounds b = new Bounds();
-
-  //   b.min.y = -1;
-  //   b.max.y = 1;
-
-  //   b.min.x = -1;
-  //   b.max.x = 1;
-
-  //   b.min.z = -1;
-  //   b.max.z = 1;
-
-  //   return b;
-  // }
 }
