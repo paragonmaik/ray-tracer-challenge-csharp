fe57d6adb40a600f4c342f8d4dede94b317aba98
maik <estevan.maik@gmail.com>
2024-01-20 14:42:26 -0300

    test: reworked viewTransformation test

--- tests/CameraFacts.cs
+++ tests/CameraFacts.cs
@@ -9,8 +9,8 @@ public class CameraFacts
     {
       Camera cam = new();
 
-      Assert.Equal(160, cam.hsize);
-      Assert.Equal(120, cam.vsize);
+      Assert.Equal(160, cam.hSize);
+      Assert.Equal(120, cam.vSize);
       Assert.Equal(Math.PI / 2, cam.fov);
     }
 
@@ -18,13 +18,14 @@ public class CameraFacts
     [InlineData(200, 125, Math.PI / 2)]
     [InlineData(125, 200, Math.PI / 2)]
     public void ValidateCanvasPixelSize(
-        int hsize, int vsize, double fov
-        )
+      int hSize,
+      int vSize,
+      double fov
+    )
     {
-      Camera cam = new(hsize, vsize, fov);
+      Camera cam = new(hSize, vSize, fov);
 
-      Assert.True(
-          Math.Abs(0.01f - cam.PixelSize) < 0.01);
+      Assert.True(Math.Abs(0.01f - cam.PixelSize) < 0.01);
     }
 
     [Fact]
@@ -34,11 +35,13 @@ public class CameraFacts
       Point from = new(0, 0, 0);
       Point to = new(0, 0, -1);
       Vector up = new(0, 1, 0);
-      Matrix expectedViewTransform = new Matrix(4)
-        .Identity();
+      Matrix expectedViewTransform = new Matrix(4).Identity();
 
-      Matrix actualViewTransform = cam
-        .ViewTransform(from, to, up);
+      Matrix actualViewTransform = cam.ViewTransform(
+        from,
+        to,
+        up
+      );
 
       Assert.Equal(expectedViewTransform, actualViewTransform);
     }
@@ -50,11 +53,17 @@ public class CameraFacts
       Point from = new(0, 0, 0);
       Point to = new(0, 0, 1);
       Vector up = new(0, 1, 0);
-      Matrix expectedViewTransform = new Matrix(4)
-        .Scale(-1, -1, -1);
+      Matrix expectedViewTransform = new Matrix(4).Scale(
+        -1,
+        -1,
+        -1
+      );
 
-      Matrix actualViewTransform = cam
-        .ViewTransform(from, to, up);
+      Matrix actualViewTransform = cam.ViewTransform(
+        from,
+        to,
+        up
+      );
 
       Assert.Equal(expectedViewTransform, actualViewTransform);
     }
@@ -63,29 +72,48 @@ public class CameraFacts
     public void ValidateViewTransformsMoveScene()
     {
       Camera cam = new();
-      Point from = new(0, 0, 0);
+      Point from = new(0, 0, 8);
       Point to = new(0, 0, 0);
       Vector up = new(0, 1, 0);
-      Matrix expectedViewTransform = new Matrix(4)
-        .Translate(0, 0, -8);
-      Matrix actualViewTransform = cam
-        .ViewTransform(from, to, up);
+
+      Matrix expectedViewTransform = new Matrix(4).Translate(
+        0,
+        0,
+        -8
+      );
+
+      Matrix actualViewTransform = cam.ViewTransform(
+        from,
+        to,
+        up
+      );
+
       Assert.Equal(expectedViewTransform, actualViewTransform);
     }
 
     [Fact]
     public void ValidateArbitraryViewTransformation()
     {
-      Camera cam = new(); Point from = new(1, 3, 2); Point to = new(4, -2, 8); Vector up = new(1, 1, 0);
-      Matrix expectedViewTransform = new(new double[4, 4]{
-       { -0.50709 , 0.50709 , 0.67612 , -2.36643 },
-       { 0.76772 , 0.60609 , 0.12122 , -2.82843 },
-       { -0.35857 , 0.59761 , -0.71714 , 0.00000 },
-       { 0.00000 , 0.00000 , 0.00000 , 1.00000 }
-      });
+      Camera cam = new();
+      Point from = new(1, 3, 2);
+      Point to = new(4, -2, 8);
+      Vector up = new(1, 1, 0);
+      Matrix expectedViewTransform =
+        new(
+          new double[4, 4]
+          {
+            { -0.50709, 0.50709, 0.67612, -2.36643 },
+            { 0.76772, 0.60609, 0.12122, -2.82843 },
+            { -0.35857, 0.59761, -0.71714, 0.00000 },
+            { 0.00000, 0.00000, 0.00000, 1.00000 }
+          }
+        );
 
-      Matrix actualViewTransform = cam
-        .ViewTransform(from, to, up);
+      Matrix actualViewTransform = cam.ViewTransform(
+        from,
+        to,
+        up
+      );
 
       Assert.Equal(expectedViewTransform, actualViewTransform);
     }
