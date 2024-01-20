9774d47a20ff4eb541af984d12be3ed647079d37
maik <estevan.maik@gmail.com>
2024-01-20 14:25:32 -0300

    feat: add IntersectableObjs.cs class

---
+++ IntersectableObjs/IntersectableObjs.cs
@@ -0,0 +1,32 @@
+﻿public class IntersectableObjs : IntersectableObject
+{
+  public bool performAABBIntersectionTest = true;
+
+  public IntersectableObjs()
+    : base() { }
+
+  public override Vector CalculateLocalNormal(
+    Point localPoint,
+    Intersection i = null
+  )
+  {
+    Console.WriteLine(
+      "Exception - Attempted to get a normal from a group object."
+    );
+    throw new NotImplementedException();
+  }
+
+  public override List<Intersection> Intersect(Ray ray)
+  {
+    List<Intersection> xs = new List<Intersection>();
+
+    Ray transRay = ray * GetMatrix().Inverse();
+
+    foreach (IntersectableObject obj in GetChildren())
+    {
+      xs.AddRange(obj.Intersect(transRay));
+    }
+
+    return Intersection.SortIntersections(xs);
+  }
+}
