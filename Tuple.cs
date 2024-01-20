df1cff565208d476c149a6a48275967feac58cf9
maik <estevan.maik@gmail.com>
2024-01-20 10:16:06 -0300

    chore: reformats Tuple.cs

--- Tuple.cs
+++ Tuple.cs
@@ -17,27 +17,30 @@
 
   public double Magnitude()
   {
-    return Math.Sqrt(this.x * this.x +
-      this.y * this.y + this.z * this.z +
-      this.w * this.w);
+    return Math.Sqrt(
+      this.x * this.x
+        + this.y * this.y
+        + this.z * this.z
+        + this.w * this.w
+    );
   }
 
   public double Dot(Tuple b)
   {
-    return this.x * b.x +
-    this.y * b.y +
-    this.z * b.z +
-    this.w * b.w;
+    return this.x * b.x
+      + this.y * b.y
+      + this.z * b.z
+      + this.w * b.w;
   }
 
   public override bool Equals(object? obj)
   {
     var tuple = obj as Tuple;
-    return tuple != null &&
-           Utility.FE(x, tuple.x) &&
-           Utility.FE(y, tuple.y) &&
-           Utility.FE(z, tuple.z) &&
-           Utility.FE(w, tuple.w);
+    return tuple != null
+      && Utility.FE(x, tuple.x)
+      && Utility.FE(y, tuple.y)
+      && Utility.FE(z, tuple.z)
+      && Utility.FE(w, tuple.w);
   }
 
   public override int GetHashCode()
