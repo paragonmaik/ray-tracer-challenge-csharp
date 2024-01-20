c3136f5f6b40b3b1b28840341ce38413054d5c6a
maik <estevan.maik@gmail.com>
2024-01-20 13:15:06 -0300

    refactor: reworks Computation.cs

--- Computations.cs
+++ Computations.cs
@@ -1,44 +1,53 @@
-public class Computations
+﻿public class Computations
 {
   public bool inside;
   public double t;
-  public IntersectableObject intersectedObject;
+  public IntersectableObject intersectableObj;
   public Point point;
-  public Vector eyeV;
-  public Vector normalV;
+  public Vector eye;
+  public Vector normal;
 
-  public Computations(Intersection i, Ray ray)
+  public Computations(
+    IntersectableObject intersectableObj,
+    double t,
+    Point point,
+    Vector eye,
+    Vector normal
+  )
   {
-    this.t = i.t;
-    this.intersectedObject = i.intersectedObj;
-    this.point = ray.Position(this.t);
-    this.eyeV = -ray.Direction();
-    this.normalV = i.intersectedObj.NormalAt(this.point);
-
-    CalculateInsideOrOutsideHit();
+    this.t = t;
+    this.intersectableObj = intersectableObj;
+    this.point = point;
+    this.eye = eye;
+    this.normal = normal;
   }
 
-  // TODO: remove
-  public void PrepareComputations(Intersection i, Ray ray)
+  public static Computations Prepare(
+    Intersection i,
+    Ray ray,
+    List<Intersection> xs
+  )
   {
-    this.t = i.t;
-    this.intersectedObject = i.intersectedObj;
-    this.point = ray.Position(this.t);
-    this.eyeV = -ray.Direction();
-    this.normalV = i.intersectedObj.NormalAt(this.point);
-    CalculateInsideOrOutsideHit();
-  }
+    Point temp = ray.Position(i.t);
 
-  private void CalculateInsideOrOutsideHit()
-  {
-    if (this.normalV.Dot(this.eyeV) < 0)
+    Computations c = new Computations(
+      i.intersectableObj,
+      i.t,
+      temp,
+      -ray.Direction().Normalize(),
+      i.intersectableObj.GetNormal(temp, i).Normalize()
+    );
+
+    if (c.normal.Dot(c.eye) < 0)
     {
-      this.inside = true;
-      this.normalV = -this.normalV;
+      c.inside = true;
+      c.normal = -c.normal;
     }
     else
     {
-      this.inside = false;
+      c.inside = false;
     }
+
+    return c;
   }
 }
