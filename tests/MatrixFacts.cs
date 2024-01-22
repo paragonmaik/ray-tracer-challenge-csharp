b035e5422dd9431eb11fac6ef1c60278fbbea0e7
maik <estevan.maik@gmail.com>
2024-01-22 11:34:27 -0300

    test: reformats MatrixFacts

--- tests/MatrixFacts.cs
+++ tests/MatrixFacts.cs
@@ -46,21 +46,24 @@ public class MatrixFacts
     [Fact]
     public void Validate4x4MatrixMultiplication()
     {
-      double[,] arr1 = {
+      double[,] arr1 =
+      {
         { 1, 1, 1, 1 },
         { 1, 1, 1, 1 },
         { 1, 1, 1, 1 },
         { 1, 1, 1, 1 }
       };
 
-      double[,] arr2 = {
+      double[,] arr2 =
+      {
         { 3, 3, 3, 3 },
         { 3, 3, 3, 3 },
         { 3, 3, 3, 3 },
         { 3, 3, 3, 3 }
       };
 
-      double[,] arr3 = {
+      double[,] arr3 =
+      {
         { 12, 12, 12, 12 },
         { 12, 12, 12, 12 },
         { 12, 12, 12, 12 },
@@ -79,19 +82,22 @@ public class MatrixFacts
     [Fact]
     public void Validate3x3MatrixMultiplication()
     {
-      double[,] arr1 = {
+      double[,] arr1 =
+      {
         { 1, 1, 1 },
         { 1, 1, 1 },
         { 1, 1, 1 }
       };
 
-      double[,] arr2 = {
+      double[,] arr2 =
+      {
         { 3, 3, 3 },
         { 3, 3, 3 },
         { 3, 3, 3 }
       };
 
-      double[,] arr3 = {
+      double[,] arr3 =
+      {
         { 9, 9, 9 },
         { 9, 9, 9 },
         { 9, 9, 9 }
@@ -109,17 +115,20 @@ public class MatrixFacts
     [Fact]
     public void Validate2x2MatrixMultiplication()
     {
-      double[,] arr1 = {
+      double[,] arr1 =
+      {
         { 1, 1 },
         { 1, 1 }
       };
 
-      double[,] arr2 = {
+      double[,] arr2 =
+      {
         { 3, 3 },
         { 3, 3 }
       };
 
-      double[,] arr3 = {
+      double[,] arr3 =
+      {
         { 6, 6 },
         { 6, 6 }
       };
@@ -136,17 +145,18 @@ public class MatrixFacts
     [Fact]
     public void ValidateMatrixMultipliedByTupl()
     {
-      double[,] arr = {
-      { -1, 4, 3, 5 },
-      { 2, 6, 8, 6 },
-      { 4, 8, 9, 6 },
-      { 9, 5, 8, 7 }
+      double[,] arr =
+      {
+        { -1, 4, 3, 5 },
+        { 2, 6, 8, 6 },
+        { 4, 8, 9, 6 },
+        { 9, 5, 8, 7 }
       };
       Matrix mat = new(arr);
-      Tupl tupl = new(2, 4, 1, 6);
-      Tupl expected = new(47, 72, 85, 88);
+      Tuple tupl = new(2, 4, 1, 6);
+      Tuple expected = new(47, 72, 85, 88);
 
-      Tupl res = mat * tupl;
+      Tuple res = mat * tupl;
 
       Assert.Equivalent(expected, res);
     }
@@ -154,17 +164,19 @@ public class MatrixFacts
     [Fact]
     public void ValidateIdentityMatrix()
     {
-      double[,] arr = {
-      { -1, 4, 3, 5 },
-      { 2, 6, 8, 6 },
-      { 4, 8, 9, 6 },
-      { 9, 5, 8, 7 }
+      double[,] arr =
+      {
+        { -1, 4, 3, 5 },
+        { 2, 6, 8, 6 },
+        { 4, 8, 9, 6 },
+        { 9, 5, 8, 7 }
       };
-      double[,] identity = {
-      { 1, 0, 0, 0 },
-      { 0, 1, 0, 0 },
-      { 0, 0, 1, 0 },
-      { 0, 0, 0, 1 }
+      double[,] identity =
+      {
+        { 1, 0, 0, 0 },
+        { 0, 1, 0, 0 },
+        { 0, 0, 1, 0 },
+        { 0, 0, 0, 1 }
       };
       Matrix mat = new(arr);
       Matrix expectedIdentity = new(identity);
@@ -177,17 +189,19 @@ public class MatrixFacts
     [Fact]
     public void ValidateTransposeMatrix()
     {
-      double[,] arr = {
-      { 0, 9, 3, 0 },
-      { 9, 8, 0, 8 },
-      { 1, 8, 5, 3 },
-      { 0, 0, 5, 8 }
-    };
-      double[,] transpose = {
-      { 0, 9, 1, 0 },
-      { 9, 8, 8, 0 },
-      { 3, 0, 5, 4 },
-      { 0, 8, 3, 8 }
+      double[,] arr =
+      {
+        { 0, 9, 3, 0 },
+        { 9, 8, 0, 8 },
+        { 1, 8, 5, 3 },
+        { 0, 0, 5, 8 }
+      };
+      double[,] transpose =
+      {
+        { 0, 9, 1, 0 },
+        { 9, 8, 8, 0 },
+        { 3, 0, 5, 4 },
+        { 0, 8, 3, 8 }
       };
       Matrix mat = new(arr);
       Matrix expectedTransposedMatrix = new(transpose);
@@ -203,9 +217,10 @@ public class MatrixFacts
     [Fact]
     public void Validate2x2MatrixDeterminant()
     {
-      double[,] arr5 = {
-      {1, 5},
-      {-3, 2}
+      double[,] arr5 =
+      {
+        { 1, 5 },
+        { -3, 2 }
       };
       double expectedDeterminant = 17;
 
@@ -218,10 +233,11 @@ public class MatrixFacts
     [Fact]
     public void Validate3x3MatrixDeterminant()
     {
-      double[,] arr = {
-      { 1, 2, 6 },
-      { -5, 8, -4 },
-      { 2, 6, 4 },
+      double[,] arr =
+      {
+        { 1, 2, 6 },
+        { -5, 8, -4 },
+        { 2, 6, 4 },
       };
 
       double expectedDeterminant = -196;
@@ -235,11 +251,12 @@ public class MatrixFacts
     [Fact]
     public void Validate4x4MatrixDeterminant()
     {
-      double[,] arr = {
-      { -2, -8, 3, 5 },
-      { -3, 1, 7, 3 },
-      { 1, 2, -9, 6 },
-      { -6, 7, 7, -9 }
+      double[,] arr =
+      {
+        { -2, -8, 3, 5 },
+        { -3, 1, 7, 3 },
+        { 1, 2, -9, 6 },
+        { -6, 7, 7, -9 }
       };
       double expectedDeterminant = -4071;
 
@@ -252,15 +269,17 @@ public class MatrixFacts
     [Fact]
     public void Validate3x3Submatrix()
     {
-      double[,] arr1 = {
-      { 1, 5, 0 },
-      { -3, 2, 7 },
-      { 0, 6, -3 }
+      double[,] arr1 =
+      {
+        { 1, 5, 0 },
+        { -3, 2, 7 },
+        { 0, 6, -3 }
       };
 
-      double[,] arr2 = {
+      double[,] arr2 =
+      {
         { -3, 2 },
-        { 0 , 6 }
+        { 0, 6 }
       };
       Matrix expectedSubmatrix = new(arr2);
 
@@ -273,17 +292,19 @@ public class MatrixFacts
     [Fact]
     public void Validate4x4Submatrix()
     {
-      double[,] arr1 = {
-      { -6, 1, 1, 6 },
-      { -8, 5, 8, 6 },
-      { -1, 0, 8, 2 },
-      { -7, 1, -1, 1 }
+      double[,] arr1 =
+      {
+        { -6, 1, 1, 6 },
+        { -8, 5, 8, 6 },
+        { -1, 0, 8, 2 },
+        { -7, 1, -1, 1 }
       };
 
-      double[,] arr2 = {
-      { -6, 1, 6 },
-      { -8, 8, 6 },
-      { -7, -1, 1 }
+      double[,] arr2 =
+      {
+        { -6, 1, 6 },
+        { -8, 8, 6 },
+        { -7, -1, 1 }
       };
 
       Matrix expectedSubmatrix = new(arr2);
@@ -297,10 +318,11 @@ public class MatrixFacts
     [Fact]
     public void Validate3x3MatrixMinor()
     {
-      double[,] arr = {
-      {0, 0, 0},
-      {1, 5, 0},
-      {-3, 2, 0}
+      double[,] arr =
+      {
+        { 0, 0, 0 },
+        { 1, 5, 0 },
+        { -3, 2, 0 }
       };
       double expectedMinor = 17;
 
@@ -313,10 +335,11 @@ public class MatrixFacts
     [Fact]
     public void Validate3x3MatrixCofactor()
     {
-      double[,] arr10 = {
-      { 3, 5, 0 },
-      { 2, -1, -7 },
-      { 6, -1, 5 }
+      double[,] arr10 =
+      {
+        { 3, 5, 0 },
+        { 2, -1, -7 },
+        { 6, -1, 5 }
       };
 
       double expectedCofactor = -25;
@@ -330,37 +353,41 @@ public class MatrixFacts
     [Fact]
     public void ValidateWhetherMatrixIsInvertible()
     {
-      double[,] arr = {
-      { -4, 2, -2, -3 },
-      { 9, 6, 2, 6 },
-      { 0, -5, 1, -5 },
-      { 0, 0, 0, 0 }
+      double[,] arr =
+      {
+        { -4, 2, -2, -3 },
+        { 9, 6, 2, 6 },
+        { 0, -5, 1, -5 },
+        { 0, 0, 0, 0 }
       };
       Matrix mat = new(arr);
 
       Action act = () => mat.Inverse();
-      NonInvertibleMatrixException exception = Assert
-        .Throws<NonInvertibleMatrixException>(act);
+      NonInvertibleMatrixException exception =
+        Assert.Throws<NonInvertibleMatrixException>(act);
 
       Assert.Equal(
-          "Determinant equals 0, Matrix cannot be inverted",
-          exception.Message);
+        "Determinant equals 0, Matrix cannot be inverted",
+        exception.Message
+      );
     }
 
     [Fact]
     public void ValidateInverseMatrix1()
     {
-      double[,] matValues = {
-       { 8, -5, 9, 2 },
-       { 7, 5, 6, 1 },
-       { -6, 0, 9, 6 },
-       { -3, 0, -9, -4 }
+      double[,] matValues =
+      {
+        { 8, -5, 9, 2 },
+        { 7, 5, 6, 1 },
+        { -6, 0, 9, 6 },
+        { -3, 0, -9, -4 }
       };
-      double[,] inverseMatValues = {
-       { -0.15385, -0.15385, -0.28205, -0.53846 },
-       { -0.07692, 0.12308, 0.02564, 0.03077 },
-       { 0.35897, 0.35897, 0.43590, 0.92308 },
-       { -0.69231, -0.69231, -0.76923, -1.92308 }
+      double[,] inverseMatValues =
+      {
+        { -0.15385, -0.15385, -0.28205, -0.53846 },
+        { -0.07692, 0.12308, 0.02564, 0.03077 },
+        { 0.35897, 0.35897, 0.43590, 0.92308 },
+        { -0.69231, -0.69231, -0.76923, -1.92308 }
       };
       Matrix mat = new(matValues);
       Matrix expectedInverseMat = new(inverseMatValues);
@@ -373,17 +400,19 @@ public class MatrixFacts
     [Fact]
     public void ValidateInverseMatrix2()
     {
-      double[,] matValues = {
-      { 9 , 3 , 0 , 9 },
-      { -5 , -2 , -6 , -3 },
-      { -4 , 9 , 6 , 4 },
-      { -7 , 6 , 6 , 2 }
+      double[,] matValues =
+      {
+        { 9, 3, 0, 9 },
+        { -5, -2, -6, -3 },
+        { -4, 9, 6, 4 },
+        { -7, 6, 6, 2 }
       };
-      double[,] inverseMatValues = {
-        { -0.04074 , -0.07778 , 0.14444 , -0.22222 },
-        { -0.07778 , 0.03333 , 0.36667 , -0.33333 },
-        { -0.02901 , -0.14630 , -0.10926 , 0.12963 },
-        { 0.17778 , 0.06667 , -0.26667 , 0.33333 }
+      double[,] inverseMatValues =
+      {
+        { -0.04074, -0.07778, 0.14444, -0.22222 },
+        { -0.07778, 0.03333, 0.36667, -0.33333 },
+        { -0.02901, -0.14630, -0.10926, 0.12963 },
+        { 0.17778, 0.06667, -0.26667, 0.33333 }
       };
       Matrix mat = new(matValues);
       Matrix expectedInverseMat = new(inverseMatValues);
@@ -396,17 +425,19 @@ public class MatrixFacts
     [Fact]
     public void ValidateInverseMatrixMultiplication()
     {
-      double[,] a = {
-        { 3 , -9 , 7 , 3 },
-        { 3 , -8 , 2 , -9 },
-        { -4 , 4 , 4 , 1 },
-        { -6 , 5 , -1 , 1 }
+      double[,] a =
+      {
+        { 3, -9, 7, 3 },
+        { 3, -8, 2, -9 },
+        { -4, 4, 4, 1 },
+        { -6, 5, -1, 1 }
       };
-      double[,] b = {
-        { 8 , 2 , 2 , 2 },
-        { 3 , -1 , 7 , 0 },
-        { 7 , 0 , 5 , 4 },
-        { 6 , -2 , 0 , 5 }
+      double[,] b =
+      {
+        { 8, 2, 2, 2 },
+        { 3, -1, 7, 0 },
+        { 7, 0, 5, 4 },
+        { 6, -2, 0, 5 }
       };
 
       Matrix matrixA = new(a);
@@ -518,16 +549,24 @@ public class MatrixFacts
     Point p = new Point(0, 1, 0);
     Matrix halfQuarter = mat.RotateXAxis(Math.PI / 4.0f);
 
-    Assert.Equal(new Point(0,
-          (float)Math.Sqrt(2.0) / 2.0f,
-          (float)Math.Sqrt(2.0) / 2.0f),
-           halfQuarter * p);
+    Assert.Equal(
+      new Point(
+        0,
+        (float)Math.Sqrt(2.0) / 2.0f,
+        (float)Math.Sqrt(2.0) / 2.0f
+      ),
+      halfQuarter * p
+    );
 
     Matrix inverseMat = halfQuarter.Inverse();
-    Assert.Equal(new Point(0,
-          (float)Math.Sqrt(2.0) / 2.0f,
-          (float)Math.Sqrt(2.0) / -2.0f),
-          inverseMat * p);
+    Assert.Equal(
+      new Point(
+        0,
+        (float)Math.Sqrt(2.0) / 2.0f,
+        (float)Math.Sqrt(2.0) / -2.0f
+      ),
+      inverseMat * p
+    );
   }
 
   [Fact]
@@ -539,10 +578,14 @@ public class MatrixFacts
     Matrix halfQuarter = mat.RotateYAxis(Math.PI / 4.0f);
     Matrix fullQuarter = mat.RotateYAxis(Math.PI / 2.0f);
 
-    Assert.Equal(new Point((float)Math.Sqrt(2.0) / 2.0f,
-          0,
-          (float)Math.Sqrt(2.0) / 2.0f),
-           halfQuarter * p);
+    Assert.Equal(
+      new Point(
+        (float)Math.Sqrt(2.0) / 2.0f,
+        0,
+        (float)Math.Sqrt(2.0) / 2.0f
+      ),
+      halfQuarter * p
+    );
     Assert.Equal(new Point(1, 0, 0), fullQuarter * p);
   }
 
@@ -554,11 +597,14 @@ public class MatrixFacts
     Point p = new Point(0, 1, 0);
     Matrix halfQuarter = mat.RotateZAxis(Math.PI / 4.0f);
 
-    Assert.Equal(new Point(
-          (float)Math.Sqrt(2.0) / 2.0f * -1.0f,
-          (float)Math.Sqrt(2.0) / 2.0f,
-          0),
-           halfQuarter * p);
+    Assert.Equal(
+      new Point(
+        (float)Math.Sqrt(2.0) / 2.0f * -1.0f,
+        (float)Math.Sqrt(2.0) / 2.0f,
+        0
+      ),
+      halfQuarter * p
+    );
   }
 
   [Fact]
@@ -567,9 +613,7 @@ public class MatrixFacts
     Matrix mat = new(4);
     Point expectedPoint = new Point(5, 3, 4);
     Point point = new Point(2, 3, 4);
-    Matrix shearMat = mat.Shear(
-              1, 0, 0, 0, 0, 0
-              );
+    Matrix shearMat = mat.Shear(1, 0, 0, 0, 0, 0);
 
     Point actualPoint = shearMat * point;
 
@@ -582,9 +626,7 @@ public class MatrixFacts
     Matrix mat = new(4);
     Point expectedPoint = new Point(6, 3, 4);
     Point point = new Point(2, 3, 4);
-    Matrix shearMat = mat.Shear(
-              0, 1, 0, 0, 0, 0
-              );
+    Matrix shearMat = mat.Shear(0, 1, 0, 0, 0, 0);
 
     Point actualPoint = shearMat * point;
 
@@ -597,9 +639,7 @@ public class MatrixFacts
     Matrix mat = new(4);
     Point expectedPoint = new Point(2, 5, 4);
     Point point = new Point(2, 3, 4);
-    Matrix shearMat = mat.Shear(
-              0, 0, 1, 0, 0, 0
-              );
+    Matrix shearMat = mat.Shear(0, 0, 1, 0, 0, 0);
 
     Point actualPoint = shearMat * point;
 
@@ -612,9 +652,7 @@ public class MatrixFacts
     Matrix mat = new(4);
     Point expectedPoint = new Point(2, 7, 4);
     Point point = new Point(2, 3, 4);
-    Matrix shearMat = mat.Shear(
-              0, 0, 0, 1, 0, 0
-              );
+    Matrix shearMat = mat.Shear(0, 0, 0, 1, 0, 0);
 
     Point actualPoint = shearMat * point;
 
@@ -627,9 +665,7 @@ public class MatrixFacts
     Matrix mat = new(4);
     Point expectedPoint = new Point(2, 3, 6);
     Point point = new Point(2, 3, 4);
-    Matrix shearMat = mat.Shear(
-              0, 0, 0, 0, 1, 0
-              );
+    Matrix shearMat = mat.Shear(0, 0, 0, 0, 1, 0);
 
     Point actualPoint = shearMat * point;
 
@@ -642,9 +678,7 @@ public class MatrixFacts
     Matrix mat = new(4);
     Point expectedPoint = new Point(2, 3, 7);
     Point point = new Point(2, 3, 4);
-    Matrix shearMat = mat.Shear(
-              0, 0, 0, 0, 0, 1
-              );
+    Matrix shearMat = mat.Shear(0, 0, 0, 0, 0, 1);
 
     Point actualPoint = shearMat * point;
 
