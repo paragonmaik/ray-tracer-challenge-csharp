95d048e4d296e90e442c6449ca65c57694ea51cf
maik <estevan.maik@gmail.com>
2024-01-22 11:36:35 -0300

    chore: reformats MaterialFacts

--- tests/MaterialFacts.cs
+++ tests/MaterialFacts.cs
@@ -7,14 +7,14 @@ public class MaterialFacts
     [Fact]
     public void ValidateMaterialinstantiation()
     {
-      Material material = new(
-          new(1, 1, 1), 0.1f, 0.9f, 0.9f, 200f);
+      Material material =
+        new(new(1, 1, 1), 0.1f, 0.9f, 0.9f, 200f);
 
       Assert.Equal(new(1, 1, 1), material.color);
       Assert.Equal(0.1f, material.Ambient);
       Assert.Equal(0.9f, material.Diffuse);
       Assert.Equal(0.9f, material.Specular);
-      Assert.Equal(200f, material.Shininess);
+      Assert.Equal(200f, material.Shinniness);
     }
   }
 }
