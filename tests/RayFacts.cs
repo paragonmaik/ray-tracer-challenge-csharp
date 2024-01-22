df703c12a41f605e6956424432528b5ee0589c82
maik <estevan.maik@gmail.com>
2024-01-22 11:56:51 -0300

    test: refactored test cases

--- tests/RayFacts.cs
+++ tests/RayFacts.cs
@@ -5,16 +5,19 @@ public class RayFacts
   public class Rays
   {
     [Theory]
-    [InlineData(0)]
-    [InlineData(1)]
-    [InlineData(-1)]
-    [InlineData(2.5)]
+    [InlineData(0, 2, 3, 4)]
+    [InlineData(1, 3, 3, 4)]
+    [InlineData(-1, 1, 3, 4)]
+    [InlineData(2.5, 4.5, 3, 4)]
     public void ValidateRayPosition(
-        double value
-        )
+      double value,
+      double px,
+      double py,
+      double pz
+    )
     {
       Ray ray = new(new(2, 3, 4), new(1, 0, 0));
-      Point expectedPosition = new();
+      Point expectedPosition = new(px, py, pz);
 
       Point actualPosition = ray.Position(value);
 
@@ -25,14 +28,14 @@ public class RayFacts
     public void ValidateIntersectionReturnsCorrectData()
     {
       Sphere sphere = new();
-      List<Intersection> intersections = new() { new(1, sphere),
-        new(2, sphere) };
+      List<Intersection> intersections =
+        new() { new(sphere, 1), new(sphere, 2) };
 
       Assert.Equal(1, intersections[0].t);
-      Assert.Equal(sphere, intersections[0].intersectedObj);
+      Assert.Equal(sphere, intersections[0].intersectableObj);
 
       Assert.Equal(2, intersections[1].t);
-      Assert.Equal(sphere, intersections[1].intersectedObj);
+      Assert.Equal(sphere, intersections[1].intersectableObj);
     }
 
     [Fact]
@@ -53,7 +56,7 @@ public class RayFacts
     public void ValidateScaleRay()
     {
       Ray ray = new(new(1, 2, 3), new(0, 1, 0));
-      Matrix mat = new Matrix(4).Scale(3, 4, 5);
+      Matrix mat = new Matrix(4).Scale(2, 3, 4);
       Point expectedOrigin = new(2, 6, 12);
       Vector expectedDirection = new(0, 3, 0);
 
@@ -76,8 +79,7 @@ public class RayFacts
     public void ValidateSetTranslateSphereMatrix()
     {
       Sphere sphere = new();
-      Matrix expectedMatrix = new Matrix(4)
-        .Translate(2, 3, 4);
+      Matrix expectedMatrix = new Matrix(4).Translate(2, 3, 4);
 
       sphere.SetMatrix(expectedMatrix);
 
